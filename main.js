e71e72ce39c31b2d0e551fbd40e25862e597af07
Félix Achucarro <94145072+Felixachucarro@users.noreply.github.com>
2023-01-10 01:09:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function () {  
+    $('#carga').fadeOut();
+    $('body').removeClass('gif');
+    
+}
+
+
+// window.alert('Esta página aún esta en construcción, es probable que haya algúnos links caidos o cosas por resolver');   
